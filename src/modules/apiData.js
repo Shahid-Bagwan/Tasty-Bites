78af11603cb4279a826bef0ed27a0b79ac9b956b
Shahid Bagwan <112220551+Shahid-Bagwan@users.noreply.github.com>
2023-08-15 19:44:58 +0530

    completed the student part

---
+++ src/modules/apiData.js
@@ -0,0 +1,4 @@
+const apiData = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=American';
+const likeUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/udgMt8FzeFXMHjSp0LMg/likes/';
+const commentUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/udgMt8FzeFXMHjSp0LMg/comments/';
+export { apiData, likeUrl, commentUrl };
